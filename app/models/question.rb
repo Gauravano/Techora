ff7b8c3a0cfd74768e4413f0522e7f1dd0175042
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-21 11:02:44 +0530

    Partial answer file initiated

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
-  validates :content,presence: true,length: {maximum: 200}
+  validates :content,presence: true,length: {minimum: 10,maximum: 200}
 end
