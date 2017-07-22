dc117ae9bed45bc35311592f57d26c75f51fec7f
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-23 02:57:48 +0530

    answers show facility added

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   validates :content,presence: true,length: {minimum: 10,maximum: 200}
+
+  def listans question_id
+    return Answer.where(question_id: question_id)
+  end
+
 end
