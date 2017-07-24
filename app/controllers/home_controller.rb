b0a5b319be8ee0b2bfa7a4d24928e0d16e97d1dc
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-24 17:10:49 +0530

    profile page with picture uploading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,8 +16,17 @@ class HomeController < ApplicationController
 
   end
 
-  def uploadImage
+  def upload_image
+    uploaded_file = params[:image]
+    filename = SecureRandom.hex+'.'+uploaded_file.original_filename.split('.')[1]
+    filepath = Dir.pwd+"/public/uploads/"+filename
+    File.open(filepath,'wb') do |file|
+      file.write(uploaded_file.read())
+    end
 
+    current_user.profile_picture = filename
+    current_user.save!
+    return redirect_to '/home/profile'
   end
 
 end
