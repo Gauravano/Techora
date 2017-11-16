2536dfa179760e126d57f4124fc8e67bcd9d7d8a
gauravano <sachdeva.gaurav1997@gmail.com>
2017-11-16 11:44:34 +0530

    some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
-class ApplicationController < ActionController::Base
-  before_action :set_paper_trail_whodunnit
 
+class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery prepend: true
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:username,:birthday, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update,keys:[:username,:birthday, :email, :password])
+  end
+
 end
